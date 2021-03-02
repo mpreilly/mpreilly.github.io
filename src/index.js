7eb9917bbebd02bfe01b12f0672848cb964ee770
mpreilly <mpreilly@gatech.edu>
2021-03-01 23:14:34 -0500

    many improvements

--- src/index.js
+++ src/index.js
@@ -24,12 +24,25 @@ const Fullpage = () => (
             <div id="name-container">
               <h1 id="main-name">Michael Reilly</h1>
             </div>
-            <div id="quote-container">
+            {/* <div id="quote-container">
               <h5 id="quote">"Simplicity is the ultimate sophistication"</h5>
+            </div> */}
+            <div id="scroll">
+              scroll
             </div>
+            <div id="nav-hint-down"></div>
+            <button class="diamond" id="diamond1" onClick={() => fullpageApi.moveTo(2, 0)}>
+              <span>GT</span>
+            </button>
+            <button class="diamond" id="diamond2" onClick={() => fullpageApi.moveTo(3, 0)}>
+              <span>J</span>
+            </button>
+            <a href="https://www.linkedin.com/in/mpreilly5/" target="_blank" id="linkedin-button">Linkedin</a>
+              
           </div>
 
           <Container className="section" id="section2">
+            <div id="nav-hint-up"></div>
             <h1 id="sec2title">Mike at Georgia Tech</h1>
             <Row id="statrow">
               <Col id="statcol1">
@@ -46,28 +59,36 @@ const Fullpage = () => (
               </Col>
             </Row>
             <p id="bonus-stat">3-time ACC Academic Honor Roll</p>
+            <div id="nav-hint-down"></div>
           </Container>
 
           <div className="section" id="project">
-            <h2 id="project-title">Personal Project: Jeopardy!</h2>
-            <h4>Play Jeopardy with the questions from full past games!</h4>
-            <p>I threw this together one finals week when I didn't have any exams.</p>
-            <hr />
-            <h5>Allows for separate devices to join the same game, using game code:</h5>
-            <ul>
-              <li>Game board to be displayed on a TV or Zoom chat, for example</li>
-              <li>3 players, with functioning real-time buzzers</li>
-              <li>A "Trebek" screen for a host to see the answers and determine if players are correct</li>
-            </ul>
-            <form target="_blank" className="jeopardy-buttons" action="https://mpreilly.github.io/jeopardy/">
-              <input type="submit" value="Go to Jeopardy" />
+            <div id="nav-hint-up"></div>
+            <div id="jeopardy-headline">
+              <h2 id="project-title">Personal Project: Jeopardy!</h2>
+              <p>Play Jeopardy with the questions from full past games! I threw this together one finals week when I didn't have any exams.</p>
+            </div>
+            <div id="jeopardy-details">
+              <h5>Using the game code, separate devices can join the same game:</h5>
+              <ul>
+                <li>Game board to be displayed on a TV or Zoom chat, for example</li>
+                <li>3 players, with functioning real-time buzzers</li>
+                <li>A "Trebek" screen for a host to see the answers and determine if players are correct</li>
+              </ul>
+            </div>
+            <div id="button-box">
+              <a href="https://mpreilly.github.io/jeopardy/" target="_blank" className="jeopardy-button">Go to Jeopardy</a>
+              <a href="https://github.com/mpreilly/jeopardy" target="_blank" className="jeopardy-button">See on Github</a>
+            </div>
+            {/* <form target="_blank" action="https://mpreilly.github.io/jeopardy/">
+              <input type="submit" className="jeopardy-button" value="Go to Jeopardy" />
             </form>
-            <form target="_blank" className="jeopardy-buttons" action="https://github.com/mpreilly/jeopardy">
-              <input type="submit" value="See on Github" />
-            </form>
+            <form target="_blank" action="https://github.com/mpreilly/jeopardy">
+              <input type="submit" className="jeopardy-button"  value="See on Github" />
+            </form> */}
           </div>
 
-          <div className="section" id="photo-section">
+          {/* <div className="section" id="photo-section">
             <div class="square img_3-3" style={{backgroundColor:'#B3A369'}} />
             <div class="square img_3-1" style={{backgroundColor:'#003057'}} />
             <div class="square img_2-3" style={{backgroundColor:'#B3A369'}} />
@@ -117,7 +138,7 @@ const Fullpage = () => (
             <div class="square img_1-3" style={{backgroundColor:'#003057'}} />
             <div class="square img_3-1" style={{backgroundColor:'#B3A369'}} />
             <div class="square img_1-2" style={{backgroundColor:'#003057'}} />
-          </div>
+          </div> */}
         </ReactFullpage.Wrapper>
       );
     }}
